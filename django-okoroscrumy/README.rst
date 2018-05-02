3b18ca08bc27112dc0f864bbf4e977b1f0dc18ef
namdi <nnamsok@gmail.com>
2018-05-02 15:10:30 +0000

    first update

---
+++ django-okoroscrumy/README.rst
@@ -0,0 +1,1 @@
+Okoroscrumy is a project management application used to assign tasks to team members and have a report about the task given.
---
+++ django-okoroscrumy/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
+
+setup(name='django-okoroscrumy',
+      version='1.0',
+      description='Okoroscrumy project management application',
+      author='Nnamdi Okoroscrumy',
+      author_email='nnamsok@gmail.com',
+      packages= find_packages()
+     )
