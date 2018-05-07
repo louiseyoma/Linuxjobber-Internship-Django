b51289f00e5f0850bce3274e4f69037a7e19f3e6
dmaaj <jamuheez2009@gmail.com>
2018-05-08 00:42:01 +0100

    made some changes to lab

--- django-jimohscrumy/jimohscrumy/urls.py
+++ django-jimohscrumy/jimohscrumy/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('goal/<int:task_id>/', views.move_goal, name="move_goal"),
     path('add_user/', views.add_user, name="add_user"),
     path('add_task/', views.add_task, name="add_task"),
-    #path('<user_id>/', views.detail, name="user_detail"),
+    path('<user_id>/', views.detail, name="user_detail"),
 ]
