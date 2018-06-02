ab710c4704c8bb49f34790f70e1996746401a8b1
louiseyoma <louiseyoma@yahoo.com>
2018-06-02 17:25:41 +0100

    cleaning master

--- eyomascrumy/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from eyomascrumy import views
-
-app_name = 'eyomascrumy'
-
-urlpatterns = [
-	path('eyomascrumy',views.homepage, name = 'homepage'),
-]
--- eyomascrumy/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, HttpResponse
-from django.http import Http404
-
-
-def homepage(request):
-	return HttpResponse('hello')
