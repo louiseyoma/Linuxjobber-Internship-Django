c1ae5308ee0028249b1847e9708818191f913e75
louiseyoma <louiseyoma@yahoo.com>
2018-06-04 22:33:52 +0100

    update

--- myscrumy/urls.py
+++ myscrumy/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url, include
+from django.urls import path
 
 urlpatterns = [
     url('admin/', admin.site.urls),
